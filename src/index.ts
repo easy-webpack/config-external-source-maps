475b9c803fabd500400935b274e73468c6397fdb
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2016-06-19 23:16:39 +0200

    feat(all): initial commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import {WebpackConfig, get} from '@easy-webpack/core'
+
+/**
+ * @param exclude add paths to packages that have problems with their sourcemaps
+ */
+export function externalSourceMaps(exclude: Array<string> = []) {
+  return function externalSourceMaps(this: WebpackConfig): WebpackConfig {
+    return {
+      module: {
+        preLoaders: get(this, 'module.preLoaders', []).concat([{
+          test: /\.jsx?$/,
+          loader: 'source-map',
+          exclude
+        }])
+      }
+    }
+  }
+}
