ae2dbf5da8ae43dfcc19323dfdea1c71c25284bd
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2016-06-24 01:39:26 +0200

    feat(index): migrate to object based parameters
    BREAKING CHANGE: method takes in an object, rather than parameters

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import {WebpackConfig, get} from '@easy-webpack/core'
 /**
  * @param exclude add paths to packages that have problems with their sourcemaps
  */
-export = function externalSourceMaps(exclude: Array<string> = []) {
+export = function externalSourceMaps({exclude = []}) {
   return function externalSourceMaps(this: WebpackConfig): WebpackConfig {
     return {
       module: {
