9d04e7019dd1c2db2251c1303278484e27a87a69
Bazyli Brzóska <bazyli.brzoska@gmail.com>
2016-10-29 03:38:13 +0200

    fix(index): use the type with metadata

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import {WebpackConfig, get} from '@easy-webpack/core'
+import {WebpackConfigWithMetadata, get} from '@easy-webpack/core'
 
 /**
  * @param exclude add paths to packages that have problems with their sourcemaps
  */
 export = function externalSourceMaps({exclude = []} = {}) {
-  return function externalSourceMaps(this: WebpackConfig): WebpackConfig {
+  return function externalSourceMaps(this: WebpackConfigWithMetadata): WebpackConfigWithMetadata {
     return {
       module: {
         preLoaders: get(this, 'module.preLoaders', []).concat([{
